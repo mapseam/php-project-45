1c156978313b59b62df48c7a54af54794df21f9a
Oleg Kartashov <mapseam@yandex.ru>
2024-01-15 18:09:55 +0300

    Step #4 is completed

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ brain-games:
 validate:
 	composer validate
 
+lint:
+	composer exec --verbose phpcs -- --standard=PSR12 src bin
+
 
 
 
--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,8 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function userGreeting(): void {
+function userGreeting(): void
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
